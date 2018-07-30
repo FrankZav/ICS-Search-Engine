8a82e1bac1d6f13fa8dd43d74f701e0470463014
FrankZav <36610518+FrankZav@users.noreply.github.com>
2018-07-29 23:27:57 -0700

    Update TermFrequency.py

--- TermFrequency.py
+++ TermFrequency.py
@@ -1,4 +1,3 @@
-#Francisco Zavalza 62706684
 import re
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
@@ -6,15 +5,20 @@ from collections import defaultdict
 from nltk.tokenize import word_tokenize
 import math
 
-
+#Total documents that are valid webpages
 validdocs = 0
+
+#Temporary Index to be imported into PyMongo
 index = defaultdict(list)
 
 
 
 def checkFiles():
+    ''' Go through the Webpage files, parse them, check if they are valid, tokenizing and save to local index '''
     global validdocs
-    false = 0
+    false = 0 #number of non webpages
+    
+    #
     for file in range(37497):
         folder = str(file/500)
         filenum = str(file%500)
@@ -22,6 +26,8 @@ def checkFiles():
         f = open(path, 'r')
         fd = f.read()
         f.close()
+        
+        #use BeautifulSoup to parse the HTML pages
         if (bool(BeautifulSoup(fd,"html.parser").find())):
             validdocs += 1
             soup = BeautifulSoup(fd, 'html.parser')
@@ -29,16 +35,21 @@ def checkFiles():
             visible_text = soup.getText()
 
             print path
+            
+            #tokenizing and set the id
             visible_text = re.sub(r'[^a-zA-Z]', " ", visible_text)
             terms = word_tokenize(visible_text)
             terms = map(lambda x: x.lower(),terms)
             id = folder + '/' + filenum
+            
+            #put all terms into a set so there are no repeats. Save Term Frequency and id for each term
             set_terms = set(terms)
             for term in set_terms:
                 term_count = terms.count(term)
                 posting = [id, float(term_count)/float(len(terms))]
                 index[term].append(posting)
         else:
+            # Finding Invalid Terms
             print(path + " IS INVALID")
             false += 1
 
@@ -46,6 +57,7 @@ def checkFiles():
 
 
 def saveToDatabase():
+    ''' Save local index to PyMongo '''
     global validdocs
     for term, postings in index.iteritems():
         df = math.log(float(validdocs)/float(len(postings)))
